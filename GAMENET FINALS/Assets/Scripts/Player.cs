4ca040740a782b41d819a32ccc8a0f310737cf61
Gerald <geraldperry.reyes@benilde.edu.ph>
2024-12-11 01:42:56 +0800

    Fixed player movement
    Note: Disable Character Controller Component on GamePlayer prefab

--- GAMENET FINALS/Assets/Scripts/Player.cs
+++ GAMENET FINALS/Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
+    /*
     [SerializeField] Transform playerCamera;
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float lookSpeed = 2f;
@@ -59,5 +60,69 @@ public class Player : NetworkBehaviour
         movement.y = _rigidbody.velocity.y;
 
         _rigidbody.velocity = movement;
+    }
+    */
+
+    [Header("Camera")]
+    public Transform playerCamera;
+    public float sens;
+    public float lookXLimit = 45f;
+    float xRotation;
+
+    [Header("Movement")]
+    public Rigidbody rb;
+    public Transform orientation;
+    public float rightInput;
+    public float forwardInput;
+    public float moveSpeed;
+    public Vector3 moveDirection;
+
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            Destroy(playerCamera.gameObject);
+            return;
+        }
+
+        rb = this.GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        CamInput();
+        MoveInput();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isLocalPlayer) return;
+
+        MovePlayer();
+    }
+
+    private void CamInput()
+    {
+        xRotation += -Input.GetAxis("Mouse Y") * sens;
+        xRotation = Mathf.Clamp(xRotation, -lookXLimit, lookXLimit);
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * sens, 0);
+    }
+
+    private void MoveInput()
+    {
+        rightInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * forwardInput + orientation.right * rightInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
     }
 }
